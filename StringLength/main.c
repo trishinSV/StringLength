0677500c94f2107d381f52a01cc43afacd6b1bf2
trishinSV <you@example.com>
2018-04-21 01:05:33 +0300

    initial

--- StringLength/main.c
+++ StringLength/main.c
@@ -7,9 +7,24 @@
 //
 
 #include <stdio.h>
-
+#include <string.h>
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    char string[128] = {};    
+    printf("Enter string");
+    gets(string);
+    int count = 0;
+    int number = 0;
+    for (int i = 0; i < strlen(string)+1; i++) {
+        if (string[i] != ' '&& string[i] != '\0' ) {
+            count++;
+            printf("%c", string[i]);
+        } else {
+            number++;
+            printf(" word #%d = %d\n", number, count);
+            count = 0;
+        }
+    }
+    getchar();
     return 0;
 }
