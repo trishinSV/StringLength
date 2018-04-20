7fde8c2d3899a23c1377016dfffae50186b08d6b
trishinSV <you@example.com>
2018-04-21 00:38:36 +0300

    Initial Commit

---
+++ StringLength/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  StringLength
+//
+//  Created by Сергей Тришин on 21.04.2018.
+//  Copyright © 2018 Сергей Тришин. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
